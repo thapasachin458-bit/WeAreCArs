6760f07f87df8ca62614bda826fbbb10a9f5e03a
Sachin Thapa <thapasachin458@gmail.com>
2026-01-03 08:46:26 +0000

    remove this image (_for element img_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,31 +1,10 @@
-import Image from 'next/image';
-import { PlaceHolderImages } from '@/lib/placeholder-images';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
 
 export default function Logo({ className }: { className?: string }) {
-  const logoImage = PlaceHolderImages.find((img) => img.id === 'we-are-cars-logo');
-
-  if (!logoImage) {
-    return (
-      <Link href="/" className={cn('text-2xl font-bold font-headline', className)}>
-        WeAreCars
-      </Link>
-    );
-  }
-
   return (
-    <Link href="/">
-      <div className={cn('relative', className)}>
-        <Image
-          src={logoImage.imageUrl}
-          alt={logoImage.description}
-          data-ai-hint={logoImage.imageHint}
-          width={200}
-          height={50}
-          className="dark:invert"
-        />
-      </div>
+    <Link href="/" className={cn('text-2xl font-bold font-headline', className)}>
+      WeAreCars
     </Link>
   );
 }
