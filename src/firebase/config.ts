c8df19a3af077a7b42b96fd7408418016a190308
Sachin Thapa <thapasachin458@gmail.com>
2026-01-04 16:00:58 +0000

    ok github is also done now i have to deploy my website on vercel i need

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-3773255922-b8e2f",
-  "appId": "1:1086718943196:web:47cc924c6affced9e43919",
-  "apiKey": "AIzaSyBc1jQPJTZ47RZzMj5fhow4xVcscI2KKk8",
-  "authDomain": "studio-3773255922-b8e2f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1086718943196"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
